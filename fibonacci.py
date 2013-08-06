34fa9d5cfd9b03dcb2de7b10d9bc9e305e7589f6
Nasogaa <adamssimon1996@gmail.com>
2013-08-06 23:44:51 +0200

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+#fibonacci
+
+def fab(x):
+    if x == 0:
+        return 0
+    if x == 1:
+        return 1
+    return fab(x-1) + fab(x-2)
+
+#main programm
+for x in range (1, 11):
+    print (fab(x))
+
